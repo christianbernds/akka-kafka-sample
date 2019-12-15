3b04cde71f3441cf77abd1b98098c7736b18bb4d
ChristianBernds <Christian.Bernds@5minds.de>
2019-12-15 21:24:09 +0100

    Add solution and empty hosts

---
+++ src/ProcessEngine.IntermediateMessageCatch.Kafka/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProcessEngine.IntermediateMessageCatch.Kafka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
